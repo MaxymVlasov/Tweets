6124c157db2c458de1e8603f52c55422b7516a83
MaxymVlasov <m.vlasov@post.com>
2016-07-29 17:03:30 +0300

    Add config for checking codestyle and Refactoring

--- console_search.py
+++ console_search.py
@@ -1,6 +1,11 @@
-#Dependencies: tweepy, pyshp
+# -*- coding: utf-8 -*-
+# Dependencies: tweepy, pyshp
 
-import json, shapefile, time, math
+import json
+import shapefile
+import time
+import math
+import urllib
 from tweepy import Stream, OAuthHandler
 from tweepy.streaming import StreamListener
 from keys import keys
@@ -10,83 +15,89 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-#Variables that contains the user credentials to access Twitter API 
+# Variables that contains the user credentials to access Twitter API
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 
-#User input
+# User input
 file_name = '20k_min'
 max_tweets = 1
-keywords = None #'#twitter, a, b, c'.split(',')
-locations = [-180,-90,180,90]
+keywords = None  # '#twitter, a, b, c'.split(',')
+locations = [-180, -90, 180, 90]
 
 
 def print_time(source, now, max, all):
     print source, ' | ', now, '/', max, ' |  Requests:', \
-         all, " |  %ssec" % math.trunc(time.time() - start_time)
+         all, ' |  %ssec' % math.trunc(time.time() - start_time)
 
 
-class listener(StreamListener):
+class Listener(StreamListener):
 
     def on_data(self, data):
-        if not hasattr(listener, '_geo_tweets'):
-            open(file_name + '.geojson', 'w').write('{"type":"FeatureCollection","features":[') #clean file
-            listener._all_tweets = 0 #from statictic
-            listener._geo_tweets = 0
-        
-        open('last_' + file_name + '.json', 'w').write(data) # Last data - for debug
-        jdata = json.loads(data)
-        listener._all_tweets += 1
+        if not hasattr(Listener, '_geo_tweets'):
+            with open(file_name + '.geojson', 'w') as file:
+                file.write('{"type":"FeatureCollection","features":[')  # Clean file
+            Listener._all_tweets = 0  # From statictic
+            Listener._geo_tweets = 0
 
-        if listener._all_tweets % 10 == 0:
-            print_time('GeoJSON', listener._geo_tweets, max_tweets, listener._all_tweets)
+        with open('last_' + file_name + '.json', 'w') as file:
+            file.write(data)  # Last data - for debug
+        jdata = json.loads(data)
+        Listener._all_tweets += 1
+
+        if Listener._all_tweets % 10 == 0:
+            print_time('GeoJSON', Listener._geo_tweets, max_tweets, Listener._all_tweets)
 
         # Skip 'limit'-messages & non geotweets
-        if 'limit' in jdata or \
-                (not jdata['place'] and not jdata['geo']):
+        if ('limit' in jdata or
+           (not jdata['place'] and not jdata['geo'])):
             return True
 
-
         # Write as geojson
-        open(file_name + '.geojson', 'a').write('\n\t{ "type": "Geotweet", \n\t\t"geometry":\n\t\t\t')
-        if not jdata['geo']: # Place
+        with open(file_name + '.geojson', 'a') as file:
+            file.write('\n\t{ "type": "Geotweet", \n\t\t"geometry":\n\t\t\t')
+        if not jdata['geo']:  # Place
             # Change Polygon to Point
             coordinates = jdata['place']['bounding_box']['coordinates'][0]
             lon = (coordinates[0][0] + coordinates[2][0]) / 2
             lat = (coordinates[0][1] + coordinates[2][1]) / 2
-            open(file_name + '.geojson', 'a').write('{"type": "Point", "coordinates": [' + \
-                str(lon) + ', ' + str(lat) + ']}')
-        else: # Geo
-            open(file_name + '.geojson', 'a').write(str(jdata["geo"]).replace("u'", '"').replace("'", '"'))
-        open(file_name + '.geojson', 'a').write(',\n\t\t"properties": \n\t\t\t' + data + '\t}')
-        
+            with open(file_name + '.geojson', 'a') as file:
+                file.write('{"type": "Point", "coordinates": [' +
+                           str(lon) + ', ' + str(lat) + ']}')
+        else:  # Geo
+            with open(file_name + '.geojson', 'a') as file:
+                file.write(str(jdata['geo']).replace("u'", '"').replace("'", '"'))
+        with open(file_name + '.geojson', 'a') as file:
+            file.write(',\n\t\t"properties": \n\t\t\t' + data + '\t}')
 
-        listener._geo_tweets += 1
+        Listener._geo_tweets += 1
 
-        if listener._geo_tweets == max_tweets:
+        if Listener._geo_tweets == max_tweets:
             open(file_name + '.geojson', 'a').write('\n]}')
-            print_time('GeoJSON', listener._geo_tweets, max_tweets, listener._all_tweets)
+            print_time('GeoJSON', Listener._geo_tweets, max_tweets, Listener._all_tweets)
             print 'GeoJSON: Done'
             return False
-        
-        open(file_name + '.geojson', 'a').write(',')
+
+        with open(file_name + '.geojson', 'a') as file:
+            file.write(',')
         return True
 
     def on_error(self, status):
         print 'ERROR ' + str(status)
         if status == 420:
-            print 'Exceed a limit requests connect to' \
-                + 'the streaming API in a window of time (15min)'
+            print 'Exceed a limit requests connect to' + \
+                  'the streaming API in a window of time (15min)'
             return False  # Returning False in on_data disconnects the stream
 
 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-def run_stream(auth, locations=[-180,-90,180,90], keywords=''):
-    stream = Stream(auth, listener())
+
+def run_stream(auth, locations=[-180, -90, 180, 90], keywords=''):
+    stream = Stream(auth, Listener())
     stream.filter(locations=locations, track=keywords)
 
 start_time = time.time()
@@ -96,73 +107,73 @@ run_stream(auth, locations, keywords)
 Generate shapefile
 """
 
+
 # function to generate .prj file information using spatialreference.org
-def getWKT_PRJ (epsg_code):
-     import urllib
-     # access projection information
-     wkt = urllib.urlopen('http://spatialreference.org/ref/epsg/{0}/prettywkt/'.format(epsg_code))
-     remove_spaces = wkt.read().replace(' ','') # remove spaces between charachters
-     output = remove_spaces.replace('\n', '') # place all the text on one line
-     return output
+def getWKT_PRJ(epsg_code):
+    # Access projection information
+    wkt = urllib.urlopen('http://spatialreference.org/ref/epsg/{0}/prettywkt/'.format(epsg_code))
+    remove_spaces = wkt.read().replace(' ', '')  # Remove spaces between charachters
+    output = remove_spaces.replace('\n', '')  # Place all the text on one line
+    return output
 
 start_time = time.time()
-shp = shapefile.Writer(shapefile.POINT) # create a point shapefile
+shp = shapefile.Writer(shapefile.POINT)  # Create a point shapefile
 
-# for every record there must be a corresponding geometry.
+# For every record there must be a corresponding geometry.
 shp.autoBalance = 1
 
-# create the field names and data type for each.
-field = [
-        'created_at', 'id', 'id_str', 'text', 'source', 'truncated', \
-        'in_reply_to_status_id', 'in_reply_to_status_id_str', \
-        'in_reply_to_user_id', 'in_reply_to_user_id_str', \
-        'in_reply_to_screen_name', 'geo', 'coordinates', \
-        'place', 'contributors', 'is_quote_status', 'retweet_count', \
-        'favorite_count', 'entities', 'favorited', 'retweeted', \
+# Create the fields names and data type for each.
+fields = [
+        'created_at', 'id', 'id_str', 'text', 'source', 'truncated',
+        'in_reply_to_status_id', 'in_reply_to_status_id_str',
+        'in_reply_to_user_id', 'in_reply_to_user_id_str',
+        'in_reply_to_screen_name', 'geo', 'coordinates',
+        'place', 'contributors', 'is_quote_status', 'retweet_count',
+        'favorite_count', 'entities', 'favorited', 'retweeted',
         'filter_level', 'lang', 'timestamp_ms'
         ]
 
-for i in range(len(field)):
-    shp.field(field[i].upper(), 'C')
+for field in fields:
+    shp.fields(field.upper(), 'C')
 
 
-# access the GeoJSON file
-with open(file_name + '.geojson') as data_file:    
+# Access the GeoJSON file
+with open(file_name + '.geojson') as data_file:
     reader = json.load(data_file)
 
 
-#loop through each of the rows and assign the attributes to variables
+# Loop through each of the rows and assign the attributes to variables
 a = {}
 for tweet in range(max_tweets):
     longitude = reader['features'][tweet]['geometry']['coordinates'][0]
     latitude = reader['features'][tweet]['geometry']['coordinates'][1]
 
-    for i in range(len(field)):
-        a.update({field[i]: str(reader['features'][tweet]['properties'][field[i]])
-                               .replace('\\', '').replace("u'", '').replace("'", '')})
+    for i in range(len(fields)):
+        a.update({fields[i]: str(reader['features'][tweet]['properties'][fields[i]])
+                 .replace('\\', '').replace("u'", '').replace("'", '')})
 
-    # create the point geometry
+    # Create the point geometry
     shp.point(float(longitude), float(latitude))
-    # add attribute data
+    # Add attribute data
 
-    shp.record(\
-        a['created_at'], a['id'], a['id_str'], a['text'], a['source'], a['truncated'], \
-        a['in_reply_to_status_id'], a['in_reply_to_status_id_str'], \
-        a['in_reply_to_user_id'], a['in_reply_to_user_id_str'], \
-        a['in_reply_to_screen_name'], a['geo'], a['coordinates'], \
-        a['place'], a['contributors'], a['is_quote_status'], a['retweet_count'], \
-        a['favorite_count'], a['entities'], a['favorited'], a['retweeted'], \
-        a['filter_level'], a['lang'], a['timestamp_ms']\
+    shp.record(
+        a['created_at'], a['id'], a['id_str'], a['text'], a['source'], a['truncated'],
+        a['in_reply_to_status_id'], a['in_reply_to_status_id_str'],
+        a['in_reply_to_user_id'], a['in_reply_to_user_id_str'],
+        a['in_reply_to_screen_name'], a['geo'], a['coordinates'],
+        a['place'], a['contributors'], a['is_quote_status'], a['retweet_count'],
+        a['favorite_count'], a['entities'], a['favorited'], a['retweeted'],
+        a['filter_level'], a['lang'], a['timestamp_ms']
         )
 
     if (tweet + 1) % 1000 == 0:
         print_time('Shapefile', tweet + 1, max_tweets, max_tweets)
 
 
-shp.save(file_name) # save the Shapefile
+shp.save(file_name)  # Save the Shapefile
 
-prj = open(file_name + '.prj', 'w') # create the .prj file
-epsg = getWKT_PRJ('4326') # call the function and supply the epsg code
-prj.write(epsg)
-prj.close()
+with open(file_name + '.prj', 'w') as prj:  # Create the .prj file
+    epsg = getWKT_PRJ('4326')  # Call the function and supply the epsg code
+    prj.write(epsg)
+
 print 'Shapefile: Done'
--- console_search2.py
+++ console_search2.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-try:
-    import json, shapefile, time
-    from datetime import datetime
-    from tweepy import Stream, OAuthHandler
-    from tweepy.streaming import StreamListener
-    from keys import keys
 
-except ImportError:
-    raise Exception("Tweepy module not installed correctly")
+import json
+import shapefile
+from datetime import datetime
+from tweepy import Stream, OAuthHandler
+from tweepy.streaming import StreamListener
+from keys import keys
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -22,7 +20,7 @@ max_tweets = 100
 stop_time = False
 stop_time_text = str(stop_time)
 
-locations = [-180,-90,180,90]
+locations = [-180, -90, 180, 90]
 
 search_method = 'Realtime (Streaming API)'
 output_metadata = 'All (need more RAM / big files)'
@@ -32,52 +30,51 @@ output_Shapefile = True
 output_QGIS = False
 
 
-class streamAPI(StreamListener):
+class StreamAPI(StreamListener):
 
     def on_data(self, data):
-        field = ''
-        shp = ''
-        if not hasattr(streamAPI, '_requests'):
-            streamAPI._requests = 0
-            streamAPI._geo_tweets = 0
+        if not hasattr(StreamAPI, '_requests'):
+            StreamAPI._requests = 0
+            StreamAPI._geo_tweets = 0
             if output_GeoJSON:
-                open(file_name + '.geojson', 'w').write('{"type":"FeatureCollection","features":[')
-            
+                with open(file_name + '.geojson', 'w') as file:
+                    file.write('{"type":"FeatureCollection","features":[')
             if output_Shapefile:
-                shp = shapefile.Writer(shapefile.POINT) # create a point shapefile
-                shp.autoBalance = 1 # for every record there must be a corresponding geometry
-                
-                # create the field names and data type for each.
+                shp = shapefile.Writer(shapefile.POINT)  # create a point shapefile
+                shp.autoBalance = 1  # for every record there must be a corresponding geometry
+
+                # create the fields names and data type for each.
                 if output_metadata == 'Minimum (created_at, text, coordinates)':
-                    field = ['created_at', 'text']
+                    fields = ['created_at', 'text']
                 elif output_metadata == 'All (need more RAM / big files)':
-                    field = [
-                        'created_at', 'id', 'id_str', 'text', 'source', 'truncated', \
-                        'in_reply_to_status_id', 'in_reply_to_status_id_str', \
-                        'in_reply_to_user_id', 'in_reply_to_user_id_str', \
-                        'in_reply_to_screen_name', 'geo', 'coordinates', \
-                        'place', 'contributors', 'is_quote_status', 'retweet_count', \
-                        'favorite_count', 'entities', 'favorited', 'retweeted', \
+                    fields = [
+                        'created_at', 'id', 'id_str', 'text', 'source', 'truncated',
+                        'in_reply_to_status_id', 'in_reply_to_status_id_str',
+                        'in_reply_to_user_id', 'in_reply_to_user_id_str',
+                        'in_reply_to_screen_name', 'geo', 'coordinates',
+                        'place', 'contributors', 'is_quote_status', 'retweet_count',
+                        'favorite_count', 'entities', 'favorited', 'retweeted',
                         'filter_level', 'lang', 'timestamp_ms']
                 elif output_metadata == 'None - Only coordinates':
-                    field = []
-                
-                for i in range(len(field)):
-                    shp.field(field[i].upper(), 'C')
+                    fields = []
+
+                for field in fields:
+                    shp.fields(field.upper(), 'C')
 
         jdata = json.loads(data)
-        streamAPI._requests += 1
-        
-        if streamAPI._requests % 50 == 0:
-            print_info(streamAPI._geo_tweets, streamAPI._requests)
-        
+        StreamAPI._requests += 1
+
+        if StreamAPI._requests % 50 == 0:
+            print_info(StreamAPI._geo_tweets, StreamAPI._requests)
+
         # Stop if timeout
-        if stop_time and \
-           stop_time.day == datetime.now().day and \
-           stop_time.hour <= datetime.now().hour and \
-           stop_time.minute <= datetime.now().minute:
-            save(output_GeoJSON, output_Shapefile, output_QGIS, shp)
-            print_info(streamAPI._geo_tweets, streamAPI._requests, 'Timeout: Done ')
+        time_now = datetime.now()
+        if (stop_time and
+           stop_time.day == time_now.day and
+           stop_time.hour <= time_now.hour and
+           stop_time.minute <= time_now.minute):
+            save_file(output_GeoJSON, output_Shapefile, output_QGIS, shp)
+            print_info(StreamAPI._geo_tweets, StreamAPI._requests, 'Timeout: Done ')
             return False
 
         # Skip 'limit'-messages & non geotweets
@@ -86,74 +83,82 @@ class streamAPI(StreamListener):
             return True
 
         # Skip tweets without keywords
-        for i in range(len(keywords)):
-            if keywords[i] in jdata['text'].lower():
+        for keyword in keywords:
+            if keyword in jdata['text'].lower():
                 break
             else:
                 return True
-
-
+        # TODO: http://vk.cc/5raROT
         geo_tweet = '\n\t{ "type": "Geotweet", \n\t\t"geometry":\n\t\t\t'
-        if not jdata['geo']: # Place
+        if not jdata['geo']:  # Place
             # Change Polygon to Point
             coordinates = jdata['place']['bounding_box']['coordinates'][0]
             lon = (coordinates[0][0] + coordinates[2][0]) / 2
             lat = (coordinates[0][1] + coordinates[2][1]) / 2
             geo_tweet += '{"type": "Point", "coordinates": [' + str(lon) + ', ' + str(lat) + ']}'
-        else: # Geo
-            geo_tweet += str(jdata["geo"]).replace("u'", '"').replace("'", '"')
-        
+        else:  # Geo
+            geo_tweet += str(jdata['geo']).replace("u'", '"').replace("'", '"')
+
         # Add metadata which user choose
         geo_tweet += ',\n\t\t"properties": \n\t\t\t'
         if output_metadata == 'Minimum (created_at, text, coordinates)':
             min_data = '"created_at":"' + str(jdata['created_at']) + '",' + \
                         '"text":"' + str(jdata['text'].encode('utf-8'))\
-                        .replace('"', "'").replace('\\', "\\\\") + '"' # crutch-escape characters
+                        .replace('"', "'").replace('\\', '\\\\') + '"'  # crutch-escape characters
             geo_tweet += '{' + min_data + '}\n'
         elif output_metadata == 'All (need more RAM / big files)':
             geo_tweet += data
         elif output_metadata == 'None - Only coordinates':
             geo_tweet += '{}'
         geo_tweet += '\t}'
-        
+
         if output_GeoJSON:
-            open(file_name + '.geojson', 'a').write(geo_tweet + ',')
+            with open(file_name + '.geojson', 'a') as file:
+                file.write(geo_tweet + ',')
         if output_Shapefile:
             # access the GeoJSON tweet
             reader = json.loads(geo_tweet)
-
             a = {}
             longitude = reader['geometry']['coordinates'][0]
             latitude = reader['geometry']['coordinates'][1]
-
-            for i in range(len(field)):
-                a.update({field[i]: str(reader['properties'][field[i]]).encode('utf-8')
-                    .replace('\\', '').replace("u'", '').replace("'", '')})
-
+            print 'geom'
+            for field in fields:
+                print str({field: str(reader['properties'][field])
+                          .replace('\\', '').replace("u'", '').replace("'", '')})
+                a.update({field: str(reader['properties'][field])
+                         .replace('\\', '').replace("u'", '').replace("'", '')})
+            print 'for'
             # create the point geometry
             shp.point(float(longitude), float(latitude))
-
             # add attribute data
             if output_metadata == 'Minimum (created_at, text, coordinates)':
                 shp.record(a['created_at'], a['text'])
             elif output_metadata == 'All (need more RAM / big files)':
-                shp.record(\
-                    a['created_at'], a['id'], a['id_str'], a['text'], a['source'], a['truncated'], \
-                    a['in_reply_to_status_id'], a['in_reply_to_status_id_str'], \
-                    a['in_reply_to_user_id'], a['in_reply_to_user_id_str'], \
-                    a['in_reply_to_screen_name'], a['geo'], a['coordinates'], \
-                    a['place'], a['contributors'], a['is_quote_status'], a['retweet_count'], \
-                    a['favorite_count'], a['entities'], a['favorited'], a['retweeted'], \
-                    a['filter_level'], a['lang'], a['timestamp_ms']\
+                shp.record(
+                    a['created_at'], a['id'], a['id_str'], a['text'], a['source'], a['truncated'],
+                    a['in_reply_to_status_id'], a['in_reply_to_status_id_str'],
+                    a['in_reply_to_user_id'], a['in_reply_to_user_id_str'],
+                    a['in_reply_to_screen_name'], a['geo'], a['coordinates'],
+                    a['place'], a['contributors'], a['is_quote_status'], a['retweet_count'],
+                    a['favorite_count'], a['entities'], a['favorited'], a['retweeted'],
+                    a['filter_level'], a['lang'], a['timestamp_ms']
                     )
             elif output_metadata == 'None - Only coordinates':
                 shp.record()
 
+        StreamAPI._geo_tweets += 1
+        if StreamAPI._geo_tweets == max_tweets:
+            if output_GeoJSON:
+                with open(file_name + '.geojson', 'a') as file:
+                    file.write('\n]}')
+            if output_Shapefile:
+                shp.save(file_name)  # save the Shapefile
 
-        streamAPI._geo_tweets += 1
-        if streamAPI._geo_tweets == max_tweets:
-            save(output_GeoJSON, output_Shapefile, output_QGIS, shp)
-            print_info(streamAPI._geo_tweets, streamAPI._requests, 'Done ')
+                with open(file_name + '.prj', 'w') as prj:  # create the .prj file
+                    epsg = getWKT_PRJ('4326')  # call the function and supply the epsg code
+                    prj.write(epsg)
+
+            print_info(StreamAPI._geo_tweets, StreamAPI._requests, 'Done ')
             return False
 
         return True
@@ -161,19 +166,19 @@ class streamAPI(StreamListener):
     def on_error(self, status):
         print 'ERROR ' + str(status), 'ERRORS'
         if status == 420:
-            print 'Exceed a limit requests connect to' + \
-            'the streaming API in a window of time (15min)', 'ERRORS'
+            print 'Exceed a limit requests connect to the streaming API' + \
+                  'in a window of time (15min)', 'ERRORS'
             return False  # Returning False in on_data disconnects the stream
 
 
 # function to generate .prj file information using spatialreference.org
-def getWKT_PRJ (epsg_code):
-     import urllib
-     # access projection information
-     wkt = urllib.urlopen('http://spatialreference.org/ref/epsg/{0}/prettywkt/'.format(epsg_code))
-     remove_spaces = wkt.read().replace(' ','') # remove spaces between charachters
-     output = remove_spaces.replace('\n', '') # place all the text on one line
-     return output
+def getWKT_PRJ(epsg_code):
+    import urllib
+    # access projection information
+    wkt = urllib.urlopen('http://spatialreference.org/ref/epsg/{0}/prettywkt/'.format(epsg_code))
+    remove_spaces = wkt.read().replace(' ', '')  # remove spaces between charachters
+    output = remove_spaces.replace('\n', '')  # place all the text on one line
+    return output
 
 
 def print_info(_geo_tweets, _requests, msg=''):
@@ -183,14 +188,15 @@ def print_info(_geo_tweets, _requests, msg=''):
         ' | Worktime: ' + str(datetime.now() - start_time)[:-7] + \
         ' | Timeout: ' + stop_time_text
 
-def save(output_GeoJSON, output_Shapefile, output_QGIS, shp):
+
+def save_file(output_GeoJSON, output_Shapefile, output_QGIS, shp):
     if output_GeoJSON:
         open(file_name + '.geojson', 'a').write('\n]}')
     if output_Shapefile:
-        shp.save(file_name) # save the Shapefile
+        shp.save(file_name)  # save the Shapefile
 
-        prj = open(file_name + '.prj', 'w') # create the .prj file
-        epsg = getWKT_PRJ('4326') # call the function and supply the epsg code
+        prj = open(file_name + '.prj', 'w')  # create the .prj file
+        epsg = getWKT_PRJ('4326')  # call the function and supply the epsg code
         prj.write(epsg)
         prj.close()
 
@@ -203,5 +209,5 @@ auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 if search_method == 'Realtime (Streaming API)':
-    stream = Stream(auth, streamAPI())
+    stream = Stream(auth, StreamAPI())
     stream.filter(locations=locations, async=True)
